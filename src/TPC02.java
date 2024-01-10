9283df16eb90ae8219b9076acfb599ed8fc5af41
THANOS <THANOS@THANOS-PC>
2024-01-10 20:10:56 +0900

    init

---
+++ src/TPC02.java
@@ -0,0 +1,12 @@
+
+public class TPC02 {
+	public static void main(String[] args) {
+		//프로그램의 3대요소 : 변수, 자료형, 할당
+		int a,b,c;
+		a = 1;
+		b = 1;
+		c = a + b;//2
+		System.out.println(c);
+		
+	}
+}
---
+++ src/TPC05.java
@@ -0,0 +1,8 @@
+
+public class TPC05 {
+	public static void main(String[] args) {
+		
+		int[] a = new int[3];
+	
+	}
+}
